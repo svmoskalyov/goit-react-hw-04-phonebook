288e0aeca9abfd42ef1a924803c25e3ed7e7cb54
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-04-03 15:08:52 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
-export const App = () => {
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { Box } from './Box';
+import { Section } from './Section/Section';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { useLocalStorage } from './hooks/useLocalStorage';
+
+export function App() {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
+
+  const addContact = ({ name, number }) => {
+    const item = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    if (contacts.find(contact => contact.name === name)) {
+      alert(`${name} is already in contacts`);
+      return;
+    } else if (contacts.find(contact => contact.number === number)) {
+      alert(`${number} is already in contacts`);
+      return;
+    } else {
+      setContacts(prevContact => [item, ...prevContact]);
+    }
+  };
+
+  const changeFilter = e => {
+    setFilter(e.target.value);
+  };
+
+  const getVisibleContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevContact =>
+      prevContact.filter(contact => contact.id !== contactId)
+    );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Box width="500px" mx="auto" p={4}>
+      <Section title="Phonebook">
+        <ContactForm addContact={addContact} />
+      </Section>
+
+      {contacts.length > 0 && (
+        <Section title="Contacts">
+          <Filter value={filter} onChange={changeFilter} />
+          <ContactList
+            contacts={getVisibleContacts()}
+            onDeleteContact={deleteContact}
+          />
+        </Section>
+      )}
+    </Box>
   );
-};
+}
